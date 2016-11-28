8732f2fc0d3e7845dda6f7d096b0be724c9184d6
Øistein Sørensen <sorenso@gmail.com>
2016-11-28 12:41:42 +0100

    Working on the animations.

--- lib/NeoPixelAnimations/NeoPixelAnimations.cpp
+++ lib/NeoPixelAnimations/NeoPixelAnimations.cpp
@@ -454,3 +454,34 @@ int NeoPixelAnimations::bouncing(int red, int green, int blue, int objectCount, 
     return bouncingPosition[0];
 }
 
+// TODO:
+//   fillFromOneEnd(r, g, b, speed);
+//   fillMixedColors() // Red, green, white // https://www.youtube.com/watch?v=VkFwQ9gey74
+//   fillDotsUntilFull
+//   fillWith2Colors // Red and white like this video: https://www.youtube.com/watch?v=76HUAiYQ6DM
+//   randomSpark with different background colors and with the blink i the same color as the background.
+//    // https://www.youtube.com/watch?v=-EkgT_sNYDY
+// https://www.youtube.com/watch?v=2W7L41D_2ns
+// https://www.youtube.com/watch?v=bb-4UADvVHw
+// http://dougalcampbell.github.io/LEDStrip/
+// http://rurandom.org/justintime/index.php?title=Driving_the_WS2811_at_800_kHz_with_an_8_MHz_AVR
+
+// fire
+// int r = 255;
+// int g = r-40;
+// int b = 40;
+//
+// for(int x = 8; x <99; x++)
+// {
+// int flicker = random(0,150);
+// int r1 = r-flicker;
+// int g1 = g-flicker;
+// int b1 = b-flicker;
+// if(g1<0) g1=0;
+// if(r1<0) r1=0;
+// if(b1<0) b1=0;
+// strip.setPixelColor(x,r1,g1, b1);
+// }
+// strip.show();
+// delay(random(50,150));
+
